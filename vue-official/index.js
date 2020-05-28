452af0fb76637fc99241cb62ec30db95121638e9
sunadoi <y.sunadoi@gmail.com>
2020-05-28 12:29:08 +0900

    vue component

---
+++ vue-official/index.js
@@ -0,0 +1,17 @@
+Vue.component('todo-item', {
+  // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+  // このプロパティは todo と呼ばれます。
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
